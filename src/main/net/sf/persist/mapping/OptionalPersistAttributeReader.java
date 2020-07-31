c85d821a4b92ebc010d8a5a32ec1c01ae48c2b18
sstirbet <sstirbet@extole.com>
2020-07-31 17:58:00 +0300

    ENG-5818 Check Optional for nulls

--- src/main/net/sf/persist/mapping/OptionalPersistAttributeReader.java
+++ src/main/net/sf/persist/mapping/OptionalPersistAttributeReader.java
@@ -1,12 +1,13 @@
 package net.sf.persist.mapping;
 
 import java.lang.reflect.Method;
+import java.util.Objects;
 import java.util.Optional;
 
 public class OptionalPersistAttributeReader implements PersistAttributeReader {
     @Override
     public Object getValue(Object instance, Method getter) throws Exception {
         Optional value = (Optional) getter.invoke(instance);
-        return value.orElse(null);
+        return Objects.nonNull(value) ? value.orElse(null) : null;
     }
 }
